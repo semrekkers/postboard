73f604f8974bf56d9d8bdf54781d10764636bb8b
Sem Rekkers <rekkers.sem@gmail.com>
2017-12-07 19:23:45 +0100

    Update app environment

--- app.env.js
+++ app.env.js
@@ -1,12 +1,25 @@
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+
 const env = {
     DEBUG: Boolean(process.env.DEBUG),
     PORT: process.env.PORT || '3000',
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/postboard',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/postboard',
+    GUARD_DISABLED: Boolean(process.env.GUARD_DISABLED),
     JWT_SECRET: process.env.JWT_SECRET || 'secret',
     INSECURE_COOKIE: Boolean(process.env.SECURE_COOKIE),
     ALLOW_SIGNUP: Boolean(process.env.ALLOW_SIGNUP),
 };
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(env.MONGODB_URI, { useMongoClient: true }, (err) => {
+    if (err) {
+        console.error('ERROR: while connecting to MongoDB: ' + err);
+        process.exit(1);
+    }
+    console.log('Connected to MongoDB');
+});
 
 module.exports = env;
