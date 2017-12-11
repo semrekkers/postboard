5ae35940e54267143a1eac9253d34580a3d9405b
Sem Rekkers <rekkers.sem@gmail.com>
2017-12-11 16:02:24 +0100

    models: Use SubDocument schema in post and user

--- models/post.js
+++ models/post.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
+const CommentSchema = new Schema({
+    user_id: { type: ObjectId, required: true },
+    date: { type: Date, required: true, default: Date.now },
+    text: { type: String }
+});
+
 const PostSchema = new Schema({
     user_id: { type: ObjectId, required: true },
     title: { type: String, required: true },
@@ -9,13 +15,7 @@ const PostSchema = new Schema({
     content: { type: String },
     edited: { type: Date },
 
-    comments: {
-        type: [{
-            user_id: { type: ObjectId, required: true },
-            date: { type: Date, required: true, default: Date.now },
-            text: { type: String }
-        }]
-    }
+    comments: { type: [CommentSchema] }
 });
 
 module.exports = mongoose.model('post', PostSchema);
--- models/user.js
+++ models/user.js
@@ -3,18 +3,19 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 const bcrypt = require('bcrypt');
 
+const FavoriteSchema = new Schema({
+    post_id: { type: ObjectId, required: true },
+    at: { type: Date, required: true, default: Date.now }
+});
+
 const UserSchema = new Schema({
     name: { type: String, required: true, index: true },
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
     hashed_password: { type: String },
+    admin: { type: Boolean },
 
-    favorites: {
-        type: [{
-            post_id: { type: ObjectId, required: true },
-            at: { type: Date, required: true, default: Date.now }
-        }]
-    }
+    favorites: { type: [FavoriteSchema] }
 });
 
 const saltRounds = 10;
