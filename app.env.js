7faa52bf6c0e1a9a01e5743cc443237d999c108d
Sem Rekkers <rekkers.sem@gmail.com>
2017-12-15 16:30:07 +0100

    app.env: Add neo4j driver

--- app.env.js
+++ app.env.js
@@ -1,19 +1,23 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
+const neo4j = require('neo4j-driver').v1;
 
-const env = {
+var env = {
     DEBUG: Boolean(process.env.DEBUG),
     PORT: process.env.PORT || '3000',
     ALLOW_ALL_ORIGINS: Boolean(process.env.ALLOW_ALL_ORIGINS),
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/postboard',
+    NEO4J_URI: process.env.NEO4J_URI || 'bolt://localhost',
+    NEO4J_USER: process.env.NEO4J_USER || 'neo4j',
+    NEO4J_PASSWD: process.env.NEO4J_PASSWD || 'neo4j',
     JWT_SECRET: process.env.JWT_SECRET || 'secret',
     JWT_EXPIRES: process.env.JWT_EXPIRES || '1h',
     ALLOW_SIGNUP: Boolean(process.env.ALLOW_SIGNUP),
 };
 
+// Setup mongoose
 mongoose.Promise = global.Promise;
-
 mongoose.connect(env.MONGODB_URI, { useMongoClient: true }, (err) => {
     if (err) {
         console.error('ERROR: while connecting to MongoDB: ' + err);
@@ -21,5 +25,10 @@ mongoose.connect(env.MONGODB_URI, { useMongoClient: true }, (err) => {
     }
     console.log('Connected to MongoDB');
 });
+
+// Setup Neo4j
+env.neo4j = neo4j.driver(env.NEO4J_URI, neo4j.auth.basic(env.NEO4J_USER, env.NEO4J_PASSWD));
+console.log('Initialized neo4j');
+
 
 module.exports = env;
